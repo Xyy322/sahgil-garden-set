7a9ef4bcc6f5314ea937d334f73c37e46621e384
Xyriz32 <xyriz23@gmail.com>
2026-05-20 02:13:22 +0800

    Clean appointment reservation wording

--- src/app/pages/AdminAppointments.tsx
+++ src/app/pages/AdminAppointments.tsx
@@ -1,14 +1,5 @@
-
 import { AppointmentsManagement } from "../components/AppointmentsManagement";
-import { CalendarCheck } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 
 export function AdminAppointments() {
-
-  return (
-    <div>
-      <AppointmentsManagement />
-    </div>
-  );
+  return <AppointmentsManagement />;
 }
-
